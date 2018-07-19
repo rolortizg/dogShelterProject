e0a3ef376b08a8b277b0cdfe05319dd177433c00
Ricardo Manzano <noteplay101@hotmail.com>
2018-07-19 15:30:22 -0500

    Demo

--- helpers/facebook.js
+++ helpers/facebook.js
@@ -7,7 +7,7 @@ const findOrCreate = require('mongoose-findorcreate');
 passport.use(new FacebookStrategy({
   clientID: "243309239830410",
   clientSecret: "da05f9012965030f77722cf0f3b2c333",
-  callbackURL: "http://localhost:3000/facebook/callback"
+  callbackURL: "https://sleepy-lowlands-81745.herokuapp.com/facebook/callback"
 },
 function(accessToken, refreshToken, profile, cb) {
   console.log(profile);
